e59d1718dfd1cde2be8639581a57097bb0400a5b
Gelson Rodrigues <GelsonMRodrigues@hotmail.com>
2016-02-15 16:04:51 -0200

    Correção da minificação;

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,20 +20,14 @@ module.exports = function(grunt){
 				},
 			}
 		},
-		copy: {
-			main: {
-				src: 'dev/gear-templates.js',
-				dest: 'dist/gear-templates.js'
-			}
-		},
 		uglify: {
 			gear: {
 				options: {
-					beautify: true,
-					mangle: true
+					// beautify: true,
+					mangle: false // Troca o nome das variáveis se for true
 				},
 				files: {
-					'dist/gear.min.js': ['dev/gear-ui.js', 'dev/gear-templates.js', 'dev/gear.js']
+					'dev/gear.min.js': ['dev/gear-ui.js', 'dev/gear-templates.js', 'dev/gear.js']
 				}
 			}
 		},
@@ -43,11 +37,30 @@ module.exports = function(grunt){
 					'dist/gear.min.css': ['dev/gear.css']
 				}
 			}
-		}
+		},
+		copy: {
+			main: {
+				files:
+					[
+						{
+							expand: true,
+							cwd: 'dev',
+							src: '*.js',
+							dest: 'dist/'
+						},
+						{
+							expand: true,
+							cwd: 'dev',
+							src: '*.css',
+							dest: 'dist/'
+						}
+					]
+			}
+		},
 	})
 	grunt.loadNpmTasks('grunt-angular-templates');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.registerTask('default', ['ngtemplates', 'copy', 'uglify', 'cssmin']);
+	grunt.registerTask('default', ['ngtemplates', 'uglify', 'cssmin', 'copy']);
 }
